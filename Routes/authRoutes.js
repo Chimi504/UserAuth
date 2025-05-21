3c55d7228030e6f8073fc169881d42bbbdfeb270
Chimi504 <lhamchimi312@gmail.com>
2025-05-21 20:53:26 +0600

    Practical 7

--- Routes/authRoutes.js
+++ Routes/authRoutes.js
@@ -1,6 +1,7 @@
 const express = require('express');
 const router = express.Router();
-const authController = require('../Controllers/authController');
+const authController = require('../controllers/authController');
+
 
 
 // root route
--- app.js
+++ app.js
@@ -5,8 +5,10 @@ const session = require('express-session');
 require('dotenv').config();
 const { createUserTable } = require('./Models/userModel');
 
+
 const app = express();
 const PORT = process.env.PORT || 3000;
+
 
 // Middlewares
 app.use(express.urlencoded({ extended: false }));
@@ -15,33 +17,32 @@ app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 app.use(session({
   secret: process.env.SESSION_SECRET || 'secretkey',
-  resave: false,
+resave: false,
   saveUninitialized: true,
 }));
+
 
 // View engine
 app.set('view engine', 'ejs');
 app.set('views', path.join(__dirname, 'views'));
 
+
 // Routes
-// const adminRoutes = require('./routes/adminRoutes');
-const authRoutes = require('./Routes/authRoutes'); // Ensure folder name is lowercase
-// const userRoutes = require('./routes/userRoutes');
+const adminRoutes = require('./Routes/adminRoutes');
+const authRoutes = require('./Routes/authRoutes');
+const userRoutes = require('./Routes/userRoutes');
 app.use('/', authRoutes);
-// app.use('/', adminRoutes);
-// app.use('/', userRoutes);
+app.use('/admin', adminRoutes);
+app.use('/user', userRoutes);
+
+
 
 // Schema creation
-(async () => {
-  try {
-    await createUserTable();
-    console.log('✅ User table checked/created.');
-  } catch (err) {
-    console.error('❌ Error creating user table:', err);
-  }
-})();
+const { createFoodTable } = require('./Models/foodModel');
+createFoodTable();
+
 
 // Server
 app.listen(PORT, () => {
-  console.log(`🚀 Server running at http://localhost:${PORT}`);
+  console.log(`Server running at http://localhost:${PORT}`);
 });
